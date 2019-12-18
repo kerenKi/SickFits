9c118833af95b4bfa7c580c7c5a18e0da7caa5f2
kerenKi <kkinberg13@gmail.com>
2019-12-18 13:00:01 +0200

    Items creation and prisma-yoga flow

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,12 @@
+const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  async items (parent, args, ctx, info) {
-    const items = await ctx.db.query.items()
-    return items
-  }
+  //forward the Api straight to the db. good for simple cases without authentication and such
+  items: forwardTo('db')
+  // async items (parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // }
 };
 
 module.exports = Query;
