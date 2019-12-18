2ee6a247bf2e33f517fb449c607391e99688a0ea
kerenKi <kkinberg13@gmail.com>
2019-12-18 12:42:32 +0200

    Making an items query

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,16 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    //create a dog!
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+  async createItem (parent, args, ctx, info) {
+    //TODO check if user is logged in
+    
+    //ctx.db access the db (we set it up in createServer.js)
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        //spreading the args object instead of copying all the fields separately 
+        ...args
+      }
+    }, info)
+
+    return item
   }
 };
 
