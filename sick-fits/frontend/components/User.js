78e5069a60fd3b2fdd54ab2cdf38fa1acde3cec1
kerenKi <kkinberg13@gmail.com>
2019-12-27 12:32:40 +0100

    Buliding User.js as a render prop component

---
+++ sick-fits/frontend/components/User.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import PropTypes from 'prop-types';
+import Error from './ErrorMessage';
+import styled from 'styled-components';
+import Head from 'next/head'
+
+
+const CURRENT_USER_QUERY = gql`
+  query CURRENT_USER_QUERY {
+    me {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const User = (props) => (
+  <Query  {...props} query={CURRENT_USER_QUERY}>
+    {payload => props.children(payload) }
+  </Query>
+)
+
+User.PropTypes = {
+  children: PropTypes.func.isRequired,
+}
+export default User;
+export { CURRENT_USER_QUERY };
