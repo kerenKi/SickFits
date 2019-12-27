f01bc4255f7aae0acf61fc65039b5c9116a9d966
kerenKi <kkinberg13@gmail.com>
2019-12-27 12:22:27 +0100

    Get user id from token flow

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,23 @@
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
+const jwt = require('jsonwebtoken');
 const db = require('./db');
 
 const server = createServer();
 
 // Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
+
+//Decode the jwt for the user id
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if(token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    req.userId = userId
+  }
+  next()
+})
 
 // TODO Use express middleware to populate current user
 
