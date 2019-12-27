97e9f59a04fdfbf6413e7226394d303734f41cef
kerenKi <kkinberg13@gmail.com>
2019-12-27 14:41:03 +0100

    Sign out component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles'
 import User from './User';
+import Signout from './Signout'
 
 function Nav(props) {
   return (
@@ -26,6 +27,7 @@ function Nav(props) {
                 <Link href="/me">
                   <a>Account</a>
                 </Link>
+                <Signout/>
               </>
             )}
             
